f71a1e04791391e3d5457666197c48a8fcf98e93
Husain Bhagat <hussainbhagat52@gmail.com>
2022-05-14 10:16:32 +0530

    Create AuthReducer.js

---
+++ src/Reducers/Auth/AuthReducer.js
@@ -0,0 +1,32 @@
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "SIGNUP":
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+    case "LOGIN":
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+        token: null,
+      };
+    case "CHECKUSER":
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+      default: 
+      return state;
+  }
+};
+
+export {authReducer};
