5a5aa21e33d5d148bd4cc63330b8a4c829dd5015
Husain Bhagat <hussainbhagat52@gmail.com>
2022-05-16 09:56:14 +0530

    Feat: Add category tabs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import Aside from '../../components/Aside/Aside';
-import VideoCard from '../../components/VideoCard/VideoCard';
-import './Home.css';
+import React from "react";
+import Aside from "../../components/Aside/Aside";
+import VideoCard from "../../components/VideoCard/VideoCard";
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <div className='content-container'>
-        <Aside></Aside>
-        <main className="main-content">
-          <div className="responsive-grid">
+    <div className="content-container">
+      <Aside></Aside>
+      <main className="main-content">
+        <div className="category-list">
+          <div className="category normal-shadow">All</div>
+          <div className="category normal-shadow">Fiction</div>
+          <div className="category normal-shadow">Non-Fiction</div>
+          <div className="category normal-shadow">Self-Help</div>
+        </div>
+        <div className="responsive-grid">
           <VideoCard></VideoCard>
           <VideoCard></VideoCard>
           <VideoCard></VideoCard>
           <VideoCard></VideoCard>
-          </div>
-        </main>
+        </div>
+      </main>
     </div>
-  )
-}
-
+  );
+};
