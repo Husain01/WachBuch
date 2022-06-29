3ae25b903ebe17d3b43f7984cc8617e2df2e2a8d
Husain Bhagat <hussainbhagat52@gmail.com>
2022-06-29 12:31:46 +0530

    Feat: Add 3 dot menu

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,29 +1,55 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import './VideoCard.css'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./VideoCard.css";
+import {
+  MdOutlineWatchLater,
+  MdPlaylistAdd,
+  MdWatchLater,
+} from "react-icons/md";
 
-const VideoCard = ({video}) => {
-    const {_id, title, creator} = video;
-    const navigate = useNavigate();
-    return (
+const VideoCard = ({ video }) => {
+  const { _id, title, creator } = video;
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  return (
     <>
-        <div className="card normal-shadow" onClick={() => navigate(`/${_id}`)}>
-            <img src={`https://i.ytimg.com/vi/${_id}/hq720.jpg`} alt="" className='card-img'/>
-            <div className="card-footer">
-                <div className="card-title">
-                    <h3 className="card-title-header">{title}</h3>
-                    <div className="card-menu">
-                    <i className="fas fa-ellipsis-v"></i>
-                    </div>
+      <div className="card normal-shadow">
+        <img
+          onClick={() => navigate(`/${_id}`)}
+          src={`https://i.ytimg.com/vi/${_id}/hq720.jpg`}
+          alt=""
+          className="card-img"
+        />
+        <div className="card-footer">
+          <div className="card-title">
+            <h3
+              className="card-title-header"
+              onClick={() => navigate(`/${_id}`)}
+            >
+              {title}
+            </h3>
+            <div className="card-menu" onClick={() => setOpen(!open)}>
+              <i className="fas fa-ellipsis-v"></i>
+              <div className={`menu-list ${open ? "" : "menu-list-hide"}`}>
+                <div>
+                  <MdOutlineWatchLater />
+                  Add to Watch Later
                 </div>
-                <div className="card-description">
-                    <h4 className='author'>{creator}</h4>
-                    <p className='vid-date'>26 Apr 2022</p>
+                <div>
+                  <MdPlaylistAdd />
+                  Add to Playlist
                 </div>
+              </div>
             </div>
+          </div>
+          <div className="card-description">
+            <h4 className="author">{creator}</h4>
+            <p className="vid-date">26 Apr 2022</p>
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
